484972fa61d88ffaefdbd17863f17bb2b4025a05
Gabriel <Gabriel.andrianoqa@gmail.com>
2024-01-16 12:35:23 -0300

    Add files via upload

---
+++ Vertical_to_Horizontal/Vertical_to_Horizontal.lua
@@ -0,0 +1,1 @@
+/run for i,v in ipairs{"Left","Right"} do for i = 2, 12 do local n = "MultiBar"..v.."Button" local btn = _G[n..i] btn:ClearAllPoints() btn:SetPoint("LEFT", n..i - 1, "RIGHT", 6, 0) end end
